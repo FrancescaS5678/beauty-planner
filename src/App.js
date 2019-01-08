74ec7eb0e6faa3a4e63dbe8c1be0b26b2b5377da
Francesca Marie Schultz <francescamschultz@gmail.com>
2019-01-07 19:04:03 -0700

    Header created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Categories from './Categories'
+import './App.css'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Categories />
+    );
+  }
+}
---
+++ src/Categories.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Categories extends Component {
+    render() {
+        return (
+            <header><h1>Beauty Planner</h1></header>
+      );
+    }
+}
