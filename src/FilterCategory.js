657e2ac16059665b7c1063fbd2f38c63b8fcec7a
Francesca Marie Schultz <francescamschultz@gmail.com>
2019-01-07 20:03:34 -0700

    Categories added

--- src/FilterCategory.js
+++ src/FilterCategory.js
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom'
 export default class Categories extends Component {
     render() {
         return (
-            <div>
+            <div className="category">
                 <h3><Link to='/addfilter'>+</Link></h3>
                 <h3><Link to='/apparel/all'>All</Link></h3>
+                <h3><Link to='/apparel/tops'>Tops</Link></h3>
+                <h3><Link to='/apparel/bottoms'>Bottoms</Link></h3>
+                <h3><Link to='/apparel/shoes'>Shoes</Link></h3>
             </div>
         );
     }
