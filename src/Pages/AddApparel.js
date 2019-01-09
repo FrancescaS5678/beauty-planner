7d59c7e2ada04db988b209c59b1b336b11de9961
Francesca Marie Schultz <francescamschultz@gmail.com>
2019-01-08 19:37:34 -0700

    All elements of add apparel page implemented

--- src/Pages/AddApparel.js
+++ src/Pages/AddApparel.js
@@ -3,6 +3,25 @@ import { Link } from 'react-router-dom'
 import SiteNav from './SiteNav'
 
 export default class AllApparel extends Component {
+
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         file: null
+    //     }
+    //     this.selectImage = this.selectImage.bind(this)
+    // }
+
+    state = {
+        file: null
+    }
+
+    selectImage = (event) => {
+        this.setState({
+            file: URL.createObjectURL(event.target.files[0])
+        })
+    }
+
     render() {
         return (
             <div>
@@ -10,9 +29,23 @@ export default class AllApparel extends Component {
                 <SiteNav />
                 <Link to="/"><button className="backButton">Back</button></Link>
                 <section>
-                    <button>Add Image</button>
+                    <h4>Add Image</h4>
+                    <input type="file" ref="fileUploader" name="addpic" onChange={this.selectImage}></input>
+                </section>
+                <section id="addApparel">
+                    <img src={this.state.file} />
+                    <form>
+                        <select name="apparel">
+                            <option value="tops">Tops</option>
+                            <option value="tottoms">Bottoms</option>
+                            <option value="thoes">Shoes</option>
+                        </select>
+                        <h4><u>Tags</u></h4>
+                        <input type="text"></input><br />
+                        <button type="submit" id="apparelSave">Save</button>
+                    </form>
                 </section>
             </div>
-      );
+        );
     }
 }
